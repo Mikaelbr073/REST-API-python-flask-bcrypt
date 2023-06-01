a1434efa51f282cd445886cf0c9f5ec7bec5d077
Mikaelbr073 <mcb3@discente.ifpe.edu.br>
2023-06-01 13:18:52 -0300

    Finalizando a implemnetação do bcrypt

--- resources/usuario.py
+++ resources/usuario.py
@@ -1,7 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.usuario import UserModel
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt
-#from werkzeug.security import safe_str_cmp
 from blacklist import BLACKLIST
 import bcrypt
 
@@ -31,11 +30,8 @@ class UserRegister(Resource):
         dados = atributos.parse_args()
         if UserModel.find_by_login(dados['login']):
             return{"messege": "The login {} already exists".format(dados['login'])}
-
-        password = dados['senha']
-        bytes = password.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hash = bcrypt.hashpw(bytes, salt)
+        password = dados['senha'].encode('utf-8')
+        hash = bcrypt.hashpw(password, bcrypt.gensalt())
         user = UserModel(dados['login'], hash)
         user.save_user()
         return {'menssage': 'User created successfully!'}, 201
@@ -45,13 +41,13 @@ class UserLogin(Resource):
     def post(cls):
         dados = atributos.parse_args()
         user = UserModel.find_by_login(dados['login'])
-        if user and user.senha == dados['senha']:
+        userPassword = dados['senha'].encode('utf-8')
+        if user and bcrypt.checkpw(userPassword, user.senha):
             token_acesso = create_access_token(identity=user.user_id)
             return {'access_token': token_acesso}, 200
-        return {'message': 'The username or pasword is incorrect.'}, 401 # Não autorizado
+        return {'message': 'The username or pasword is incorrect.'}, 401 
 
 class UserLogout(Resource):
-
     @jwt_required()
     def post(self):
         jwt_id = get_jwt()['jti'] #JWT Token Indetifier
